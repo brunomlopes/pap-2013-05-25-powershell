554fa912083b7e765fdb8ac4a5ec6cfee2b24b59
Bruno Lopes <brunomlopes@gmail.com>
2013-05-28 14:47:44 +0200

    Update settings.ps1
    typo

--- 2-push/settings.ps1
+++ 2-push/settings.ps1
@@ -1,6 +1,6 @@
 [Microsoft.ServiceBus.ServiceBusEnvironment]::SystemConnectivity.Mode = 'http'
 
-$settings_connectionstring = "<connectino string here>";
+$settings_connectionstring = "<connection string here>";
 $settings_namespace = "<message queue namespace here>";
 $settings_name = "owner"
 $settings_key = "<key here>";
